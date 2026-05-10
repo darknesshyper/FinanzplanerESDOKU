8661e980df3bcb4dae73b4b759bd78a7f048799e
darknesshyper <david-chris.kuder@web.de>
2026-05-10 21:56:48 +0200

    added backend logic for income based on existing

---
+++ backend/dao/einnahmeDao.js
@@ -0,0 +1,93 @@
+const helper = require('../helper.js');
+
+class EinnahmeDao {
+
+    constructor(dbConnection) {
+        this._conn = dbConnection;
+    }
+    getConnection() {
+        return this._conn;
+    }
+
+    loadById(id) {
+        var sql = 'SELECT * FROM Einnahme WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var result = statement.get(id);
+
+        if (helper.isUndefined(result)) 
+            throw new Error('No Record found by id=' + id);
+
+        return helper.objectKeysToLower(result);
+    }
+
+    loadAll() {
+        var sql = 'SELECT * FROM Einnahme';
+        var statement = this._conn.prepare(sql);
+        var result = statement.all();
+
+        if (helper.isArrayEmpty(result)) 
+            return [];
+
+        for (var i = 0; i < result.length; i++) {
+            result[i] = helper.objectKeysToLower(result[i]);
+        }
+
+        return result;
+    }
+
+    exists(id) {
+        var sql = 'SELECT COUNT(id) AS cnt FROM Einnahme WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var result = statement.get(id);
+
+        if (result.cnt == 1) 
+            return true;
+
+        return false;
+    }
+
+    create(bezeichnung = '', geldbetrag = 0, kategorie = '', datum = '') {
+        var sql = 'INSERT INTO Einnahme (bezeichnung, geldbetrag, kategorie, datum) VALUES (?,?,?,?)';
+        var statement = this._conn.prepare(sql);
+        var params = [bezeichnung, geldbetrag, kategorie, datum];
+        var result = statement.run(params);
+
+        if (result.changes != 1) 
+            throw new Error('Could not insert new Record. Data: ' + params);
+
+        return this.loadById(result.lastInsertRowid);
+    }
+
+    update(id, bezeichnung = '', geldbetrag = 0, kategorie = '', datum = '') {
+        var sql = 'UPDATE Einnahme SET bezeichnung=?, geldbetrag=?, kategorie=?, datum=? WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var params = [bezeichnung, geldbetrag, kategorie, datum, id];
+        var result = statement.run(params);
+
+        if (result.changes != 1) 
+            throw new Error('Could not update existing Record. Data: ' + params);
+
+        return this.loadById(id);
+    }
+
+    delete(id) {
+        try {
+            var sql = 'DELETE FROM Einnahme WHERE id=?';
+            var statement = this._conn.prepare(sql);
+            var result = statement.run(id);
+
+            if (result.changes != 1) 
+                throw new Error('Could not delete Record by id=' + id);
+
+            return true;
+        } catch (ex) {
+            throw new Error('Could not delete Record by id=' + id + '. Reason: ' + ex.message);
+        }
+    }
+
+    toString() {
+        console.log('EinnahmeDao [_conn=' + this._conn + ']');
+    }
+}
+
+module.exports = EinnahmeDao;
---
+++ backend/services/einnahme.js
@@ -0,0 +1,117 @@
+const helper = require('../helper.js');
+const EinnahmeDao = require('../dao/einnahmeDao.js');
+const express = require('express');
+var serviceRouter = express.Router();
+
+console.log('- Service Einnahme');
+
+serviceRouter.get('/einnahme/gib/:id', function(request, response) {
+    console.log('Service Einnahme: Client requested one record, id=' + request.params.id);
+
+    const einnahmeDao = new EinnahmeDao(request.app.locals.dbConnection);
+    try {
+        var obj = einnahmeDao.loadById(request.params.id);
+        console.log('Service Einnahme: Record loaded');
+        response.status(200).json(obj);
+    } catch (ex) {
+        console.error('Service Einnahme: Error loading record by id. Exception occured: ' + ex.message);
+        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
+    }
+});
+
+serviceRouter.get('/einnahme/alle', function(request, response) {
+    console.log('Service Einnahme: Client requested all records');
+
+    const einnahmeDao = new EinnahmeDao(request.app.locals.dbConnection);
+    try {
+        var arr = einnahmeDao.loadAll();
+        console.log('Service Einnahme: Records loaded, count=' + arr.length);
+        response.status(200).json(arr);
+    } catch (ex) {
+        console.error('Service Einnahme: Error loading all records. Exception occured: ' + ex.message);
+        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
+    }
+});
+
+serviceRouter.get('/einnahme/existiert/:id', function(request, response) {
+    console.log('Service Einnahme: Client requested check, if record exists, id=' + request.params.id);
+
+    const einnahmeDao = new EinnahmeDao(request.app.locals.dbConnection);
+    try {
+        var exists = einnahmeDao.exists(request.params.id);
+        console.log('Service Einnahme: Check if record exists by id=' + request.params.id + ', exists=' + exists);
+        response.status(200).json({'id': request.params.id, 'existiert': exists});
+    } catch (ex) {
+        console.error('Service Einnahme: Error checking if record exists. Exception occured: ' + ex.message);
+        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
+    }
+});
+
+serviceRouter.post('/einnahme', function(request, response) {
+    console.log('Service Einnahme: Client requested creation of new record');
+
+    var errorMsgs=[];
+    if (helper.isUndefined(request.body.bezeichnung)) 
+        errorMsgs.push('bezeichnung fehlt');
+    if (helper.isUndefined(request.body.geldbetrag)) {
+        errorMsgs.push('geldbetrag fehlt');
+    } else if (!helper.isNumeric(request.body.geldbetrag)) {
+        errorMsgs.push('geldbetrag muss eine Zahl sein');
+    }
+   if (helper.isUndefined(request.body.kategorie)) {
+        errorMsgs.push('kategorie fehlt');
+    } else {
+        const allowedCategories = [
+            'Gehalt/Lohn',
+            'Nebenjob',
+            'Bonus/Prämien',
+            'Zinsen',
+            'Mieteinnahmen',
+            'Staatliche Leistungen',
+            'Geschenke',
+            'Sonstiges'
+        ];
+        
+        if (!allowedCategories.includes(request.body.kategorie)) {
+            errorMsgs.push('kategorie ungültig. Erlaubte Werte: ' + allowedCategories.join(', '));
+        }
+    }
+    if (helper.isUndefined(request.body.datum)) 
+        errorMsgs.push('datum fehlt');
+    
+    if (errorMsgs.length > 0) {
+        console.log('Service Einnahme: Creation not possible, data missing');
+        response.status(400).json({ 'fehler': true, 'nachricht': 'Funktion nicht möglich. Fehlende Daten: ' + helper.concatArray(errorMsgs) });
+        return;
+    }
+
+    const einnahmeDao = new EinnahmeDao(request.app.locals.dbConnection);
+    try {
+        var obj = einnahmeDao.create(request.body.bezeichnung, request.body.geldbetrag, request.body.kategorie.id);
+        console.log('Service Einnahme: Record inserted');
+        response.status(200).json(obj);
+    } catch (ex) {
+        console.error('Service Einnahme: Error creating new record. Exception occured: ' + ex.message);
+        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
+    }    
+});
+
+
+
+
+serviceRouter.delete('/einnahme/:id', function(request, response) {
+    console.log('Service Einnahme: Client requested deletion of record, id=' + request.params.id);
+
+    const einnahmeDao = new EinnahmeDao(request.app.locals.dbConnection);
+    try {
+        var obj = einnahmeDao.loadById(request.params.id);
+        einnahmeDao.delete(request.params.id);
+        console.log('Service Einnahme: Deletion of record successfull, id=' + request.params.id);
+        response.status(200).json({ 'gelöscht': true, 'eintrag': obj });
+    } catch (ex) {
+        console.error('Service Einnahme: Error deleting record. Exception occured: ' + ex.message);
+        response.status(400).json({ 'fehler': true, 'nachricht': ex.message });
+    }
+});
+
+module.exports = serviceRouter;
